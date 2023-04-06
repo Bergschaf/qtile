67d936148310b33790e636c06bd2dbbecd6c2f42
bergschaf <christian.k@netcom-mail.de>
2023-04-06 18:59:22 +0200

    conifg

---
+++ locate_config_files.py
@@ -0,0 +1,27 @@
+import os
+
+CONFIG_DIR = "~/.config/qtile/configs"
+config = [
+    ("alacrity.yml", "~/.config/alacrity/alacrity.yml"),
+    ("config.fish", "~/.config/fish/config.fish"),
+    ("starship.toml", "~/.config/starship.toml"),
+]
+
+
+def cp_here():
+    input("Do you want to copy the config files to the current directory and overwrite them? (y/n) ")
+    # backup config dir
+    os.system(f"cp -r {CONFIG_DIR} {CONFIG_DIR}_backup")
+
+    for file in config:
+        os.system(f"cp {file[1]} {os.path.join(CONFIG_DIR, file[0])}")
+
+    print("Done!")
+
+def cp_there():
+    for file in config:
+        os.system(f"cp {os.path.join(CONFIG_DIR, file[0])} {file[1]}")
+
+if __name__ == '__main__':
+    #cp_here(
+    cp_there()
