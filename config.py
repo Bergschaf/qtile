ddab82f99bf22cca233c8b3e45dc865b54721d82
bergschaf <christian.k@netcom-mail.de>
2023-04-08 16:59:57 +0200

    battery bidde

--- config.py
+++ config.py
@@ -204,8 +204,8 @@ screens = [
                 widget.GroupBox(),
                 widget.Prompt(),
                 widget.WindowName(),
-                widget.Wlan(interface="wlp3s0", format="{essid} {percent:2.0%}"),
-                widget.Systray(),
+                #widget.Wlan(interface="wlp3s0", format="{essid} {percent:2.0%}"),
+                #widget.Systray(),
                 widget.Clock(padding=30),
 
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
@@ -238,6 +238,7 @@ screens = [
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Systray(),
+                widget.Battery(),
                 widget.Clock(padding=30),
 
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
